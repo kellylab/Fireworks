b27b6d628da8c8ffc32fce1e2d5ca2c54b932021
Saad Khan <skhan8@mail.einstein.yu.edu>
2018-06-23 18:16:03 -0400

    progress on cache class
    I implemented a dummycache class for testing

--- Fireworks/cache.py
+++ Fireworks/cache.py
@@ -1,29 +1,43 @@
 from Fireworks.message import Message
 from abc import abstractmethod
+import pandas as pd
+from itertools import count
+
 # caches = {
 #     'LRU': cachetools.LRUCache,
 # }
 
 class MessageCache:
+    """
+    A message cache stores parts of a larger method and supports retrievals and
+    insertions based on index.
+    The use case for a MessageCache is for storing parts of a large dataset in memory.
+    The MessageCache can keep track of which elmeents and which indices are present
+    in memory at a given time and allow for updates and retrievals.
+    """
 
     def __init__(self, max_size):
         self.max_size = max_size
         self.cache = Message()
+        # Initialize a list of pointers to keep track of internal indices
+        self.pointers = pd.Series({-1:-1}, dtype=int)
+        del self.pointers[-1]
 
     def __getitem__(self, index):
-        if type(index) is int:
-            index = slice(index, index+1)
 
-        cache_indices = self.get_indices(index)
+        if type(index) is slice:
+            index = slice_to_list(index)
 
-        return self.read_cache(cache_indices)
+        cache_indices = self.indices[index] # This will raise an error if index not present
+        # try:
+        #     cache_indices = self.indices[index]
+        # except KeyError as e:
+        #     raise KeyError("Index {0} not in cache.".format(e))
+
+        return self.cache[cache_indices]
 
     @abstractmethod
-    def __setitem__(self, index, message):
-        if type(index) is int:
-            pass
-        if type(index) is slice:
-            pass
+    def __setitem__(self, index, message): pass
 
     def __getattr__(self, *args, **kwargs):
         return self.cache.__getattr__(args, kwargs)
@@ -31,14 +45,64 @@ class MessageCache:
     def search(self, **kwargs):
         pass
 
-    def free(self, n): 
-        """ Ensure there is enough free space for n elements. """
-        free_space = self.max_size - self.size
-        if n > free_space:
-            self._free(n-free_space)
-
-    def _free(self, n): pass
+    # def free(self, n):
+    #     """ Ensure there is enough free space for n elements. """
+    #     free_space = self.max_size - self.size
+    #     if n > free_space:
+    #         self._free(n-free_space)
+    #
+    # def _free(self, n): pass
 
     @property
     def size(self):
         return len(self.cache)
+
+class DummyCache:
+    """
+    This is a basic implementation of a MessageCache that simply appends new
+    elements and never clears memory internally
+    """
+
+    def __setitem__(self, index, message):
+        if type(index) is int:
+            index = [index]
+        if type(index) is slice:
+            index = slice_to_list(index)
+        present = index.intersection(self.pointers)
+        not_present = index.difference(self.pointers)
+        # Get indices in message
+        present_indices = get_indices(index, present)
+        not_present_indices = get_indices(index, not_present)
+        self.add_new(present, message[present_indices])
+        self.update_existing(not_present, message[not_present_indices])
+
+    def add_new(self, index, message):
+        """
+        Adds new elements to cache and updates pointers
+        """
+        start = len(self.cache)
+        self.cache.append(message)
+        stop = len(self.cache)
+        self.pointers[index] = [i for i in range(start,stop)]
+
+    def update_exsiting(self, index, message):
+        """
+        Updates elements already in the message
+        """
+        indices = self.pointers[index]
+        self.cache[indices] = data 
+
+def slice_to_list(s):
+    """
+    Converts a slice object to a list of indices
+    """
+    step = s.step or 1
+    start = s.start
+    stop = s.stop
+    return [x for x in range(start,stop,step)]
+
+def get_indices(values, listlike):
+    """
+    Returns the indices in litlike that match elements in values
+    """
+    return [i if l in values for i,l  in zip(count, listlike)]
--- Fireworks/cache_test.py
+++ Fireworks/cache_test.py
@@ -1,6 +1,16 @@
-from Fireworks.cache import MessageCache
+from Fireworks import cache
 
-def test_cache(): pass
+def test_slice_to_list():
+    a = slice(0,10)
+    l = cache.slice_to_list(a)
+    assert l == [0,1,2,3,4,5,6,7,8,9]
+    a = slice(0,10,2)
+    l = cache.slice_to_list(a)
+    assert l == [0,2,4,6,8]
+
+def test_init():
+    m = cache.MessageCache(10)
+    assert m.max_size == 10
 
 def test_insert(): pass
 
