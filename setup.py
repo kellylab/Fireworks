bc91842c2e5895c9344ca0299712304c62e4bbad
Saad Khan <skhan8@mail.einstein.yu.edu>
2018-08-25 14:17:35 -0400

    Updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Fireworks',
-    version='0.1.4',
+    version='0.1.5',
     packages=['Fireworks'],
     long_description=open('README.md').read()
 )
