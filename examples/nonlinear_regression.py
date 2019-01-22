040b3f3fb4e18f0b6fceeeea14604eeffa8b899a
Saad Khan <skhan8@mail.einstein.yu.edu>
2019-01-22 16:42:07 -0500

    Began Working on Examples
    Examples that will be made:
    - Nonlinear regression (to demonstrate basics and hyperparameter optimization)
    - Diabetic retinopathy (to demonstrate a CNN training, model freezing, and stacking models. We will use a pretrained ImageNet as a base)
    - Reinforcement learning (to demonstrate a non-trivial computation graph in action)
    - Biovectors (to demonstrate an NLP example)

---
+++ examples/nonlinear_regression.py
@@ -0,0 +1,38 @@
+import Fireworks
+from Fireworks import Message
+import numpy as np
+from random import randint
+
+def generate_data(n=1000):
+
+    a = randint(-10,10)
+    b = randint(-10,10)
+    c = randint(-10,10)
+    errors = np.random.normal(0, .5, n)
+    x = np.random.rand(n)*100
+    y = a + b*x + c*x**2
+
+    return Message({'x': x, 'y': y,}), {'errors': errors, 'a': a, 'b': b, 'c': c}
+
+class NonlinearModel(Model):
+
+    required_components = ['a','b', 'c', 'd', 'e']
+
+    def init_default_components(self):
+
+        pass
+
+    def forward(self, message):
+
+        x = message[self.in_column]
+        message[self.out_column] = self.a + self.b*x + self.c*x**2 + self.d*x**3 + self.e*x**4
+
+        return message
+
+# Construct training data, save into database, and get get_minibatcher
+
+# Construct training closure and train using ignite
+
+# Specify parameters and metrics construction. Initialize Experiment(s).
+
+# Specify hyperparameter optimization scheme using Factory
