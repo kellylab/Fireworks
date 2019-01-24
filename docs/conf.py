c657850a0789a3d2524ac5ec952504e76f92f9c8
Saad Khan <skhan8@mail.einstein.yu.edu>
2019-01-24 17:31:01 -0500

    Update docs and progress on database

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Saad Khan'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.2.8'
 
 
 # -- General configuration ---------------------------------------------------
